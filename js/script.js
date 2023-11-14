feb741548e35291c828b976249ae2ec7b1dff7f0
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-14 16:39:13 +0100

    Fix: thumbnails WIP

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* circular index array: 
+/* Array per l'indice circolare:
 const imageData = [ 'image1.png', 'img2.png', 'img3.png' ];
 let currentImage = 0;
 
@@ -6,77 +6,77 @@ const nextImage = () => currentImage = (currentImage + 1) % imageData.length;
 
 const prevImage = () => currentImage = (currentImage - 1 + imageData.length) % imageData.length;*/
 
-
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 const app = createApp({
-    data(){
-        return{
+    data() {
+        return {
             slides: [
-                {image: 'img/01.webp',
-                title: 'Marvel\'s Spiderman Miles Morale',
-                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-            }, 
-            {
-                image: 'img/02.webp',
-                title: 'Ratchet & Clank: Rift Apart',
-                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-            }, 
-            {
-                image: 'img/03.webp',
-                title: 'Fortnite',
-                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            }, 
-            {
-                image: 'img/04.webp',
-                title: 'Stray',
-                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            }, 
-            {
-                image: 'img/05.webp',
-                title: "Marvel's Avengers",
-                text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-            }
-
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Vivi la crescita di Miles Morales, il nuovo eroe che padroneggia incredibili ed esplosivi poteri per diventare il proprio Spider-Man.',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Esplora dimensioni con Ratchet and Clank mentre affrontano un malvagio imperatore proveniente da un\'altra realtà.',
+                },
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Unisci tutti i tuoi amici e tuffati in Fortnite di Epic Games, un massiccio scontro a 100 giocatori che unisce saccheggi, creazioni, sparatorie e caos.",
+                },
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Perso, ferito e solo, un gatto randagio deve districare un antico mistero per sfuggire a una città dimenticata da tempo.',
+                },
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers è un epico gioco d\'azione e avventura in terza persona che unisce una storia originale e cinematografica a un gameplay in singolo e cooperativo.',
+                }
             ],
-            activeIndex : 0,
-
+            activeIndex: 0,
         };
     },
-    //funzioni:
+    // Funzioni:
     methods: {
-        //per settare l'index attivo
+        // Per impostare l'indice attivo
         setActiveIndex(index) {
             this.activeIndex = index;
         },
-        //circular index per next slide 
+        // Indice circolare per la slide successiva
         nextSlide() {
             this.activeIndex = (this.activeIndex + 1) % this.slides.length;
         },
-        prevSlide(){
-            //circular index per prev slide
+        // Indice circolare per la slide precedente
+        prevSlide() {
             this.activeIndex = (this.activeIndex - 1 + this.slides.length) % this.slides.length;
         },
+        // Metti in pausa l'autoplay
         pauseAutoplay() {
             clearInterval(this.autoplayInterval);
         },
-        resumeAutoplay () {
+        // Riprendi l'autoplay
+        resumeAutoplay() {
             this.autoplayInterval = setInterval(() => {
                 this.nextSlide();
             }, 3000);
         }
     },
-    mounted () {
-        this.autoplayInterval = setIterval(() => {
+    mounted() {
+        // Inizia l'autoplay alla creazione dell'app
+        this.autoplayInterval = setInterval(() => {
             this.nextSlide();
         }, 3000);
     },
-    beforeUnmount(){
+    beforeUnmount() {
+        // Pulisci l'intervallo
         clearInterval(this.autoplayInterval);
     }
 });
 
+// Monta l'applicazione all'elemento con id "app"
 app.mount(`#app`);
-
-
-
